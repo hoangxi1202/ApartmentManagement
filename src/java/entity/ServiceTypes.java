53f6f58fa17f580abce9449f8739ffc1cfc13747
hoangxi1202 <huynhdominhhoang6601@gmail.com>
2022-06-12 03:17:37 +0700

    add

--- src/java/entity/ServiceTypes.java
+++ src/java/entity/ServiceTypes.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 package entity;
@@ -36,42 +35,3 @@ public class ServiceTypes {
     }
     
 }
-=======
-
-
-package entity;
-
-/**
- *
- * @author Minh Hoàng
- */
-public class ServiceTypes {
-    private String typeId;
-    private String typeName;
-
-    public String getTypeId() {
-        return typeId;
-    }
-
-    public void setTypeId(String typeId) {
-        this.typeId = typeId;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
-    public ServiceTypes() {
-    }
-
-    public ServiceTypes(String typeId, String typeName) {
-        this.typeId = typeId;
-        this.typeName = typeName;
-    }
-    
-}
->>>>>>> 7b4d225f2769ab275df2795250f54860ef0cf62e
